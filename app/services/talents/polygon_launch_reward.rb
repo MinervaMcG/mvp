be2286acdcf39118459f5fabd44b761bf4482604
Pallavi Gupta <guptapallavi955@gmail.com>
2022-10-17 00:06:09 +0530

    Minor changes to reward service

--- app/services/talents/polygon_launch_reward.rb
+++ app/services/talents/polygon_launch_reward.rb
@@ -2,20 +2,15 @@ require "web3/api_proxy"
 
 module Talents
   class PolygonLaunchReward
-    def initialize
-      @contract_env = ENV["CONTRACTS_ENV"]
-      @chain_id = chain_id
-    end
-
     def call(user)
-      polygon_tokens_count = TalentToken.deployed.on_chain(@chain_id).count
+      polygon_tokens_count = TalentToken.deployed.on_chain(chain_id).count
       Reward.create!(user: user, amount: 500, category: "race") if polygon_tokens_count <= 50
     end
 
     private
 
     def chain_id
-      network = Web3::ApiProxy.const_get("#{@contract_env == "production" ? "" : "TESTNET"}_POLYGON_CHAIN")
+      network = Web3::ApiProxy.const_get("#{ENV["CONTRACTS_ENV"] == "production" ? "" : "TESTNET_"}POLYGON_CHAIN")
       network[2].to_i
     end
   end
