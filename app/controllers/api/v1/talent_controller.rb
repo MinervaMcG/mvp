1b7524e138629334bb3cfe1cae4658e4c6a302aa
Pallavi Gupta <guptapallavi955@gmail.com>
2022-11-09 22:30:22 +0530

    Fix syntax error

--- app/controllers/api/v1/talent_controller.rb
+++ app/controllers/api/v1/talent_controller.rb
@@ -3,7 +3,7 @@ class API::V1::TalentController < ApplicationController
   PAGE_NEUTRALIZER = 1
 
   def index
-    paging, talents = Talents::ChewySearch.new(filter_params: filter_params.to_h, admin: admin_or_moderator: current_user.admin_or_moderator?, size: per_page, from: ((params[:page] || PAGE_NEUTRALIZER).to_i - PAGE_NEUTRALIZER) * per_page, current_user_watchlist: current_user_watchlist).call
+    paging, talents = Talents::ChewySearch.new(filter_params: filter_params.to_h, admin_or_moderator: current_user.admin_or_moderator?, size: per_page, from: ((params[:page] || PAGE_NEUTRALIZER).to_i - PAGE_NEUTRALIZER) * per_page, current_user_watchlist: current_user_watchlist).call
     render json: {talents: talents, pagination: paging}, status: :ok
   end
 
